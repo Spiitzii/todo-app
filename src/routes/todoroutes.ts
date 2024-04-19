d1645930750ffd74740ed9a872d13413c134a42e
Spiitzii <spitzi@online.de>
2024-04-19 20:25:14 +0200

    setup router

---
+++ src/routes/todoroutes.ts
@@ -0,0 +1,12 @@
+import express from 'events';
+import todoController from '../controllers/todocontroller';
+
+const router = express.Router();
+
+router.get('/', todoController.getAllTodos);
+router.get('/:id', todoController.getTodoById);
+router.post('/', todoController.createTodo);
+router.put('/:id', todoController.updateTodo);
+router.delete('/:id', todoController.deleteTodo);
+
+export default router;
